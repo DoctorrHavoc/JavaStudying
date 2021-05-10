6f49692d749055127af21f050dadb06f769a3922
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2021-05-11 00:28:26 +0300

    Update sleepIn.java

--- CodingBatEx/Warmup-1/sleepIn.java
+++ CodingBatEx/Warmup-1/sleepIn.java
@@ -1,10 +1,26 @@
+/* The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in
 
-  public boolean sleepIn(boolean weekday, boolean vacation) {
-			if(weekday == false && vacation == false)
-        return true;
-			else if(weekday == true && vacation == false) 
-        return false;
-    	else
-     		return true; 
-             
+
+        sleepIn(false, false) → true
+        sleepIn(true, false) → false
+        sleepIn(false, true) → true
+ */
+public class Code1 {
+    public boolean sleepIn(boolean weekday,boolean vacation){
+        if(weekday == false && vacation == false)
+            return true;
+        else if(weekday == true && vacation == false)
+            return false;
+        else return true;
+    }
 }
+
+/*
+possible solution:
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+    return true;
+  }
+ */
