2396801aa3c00bd6e1c7fd1aed21856feb313efd
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2021-05-12 23:43:47 +0300

    Create nearHundred.java

---
+++ CodingBatEx/Warmup-1/nearHundred.java
@@ -0,0 +1,16 @@
+/*
+
+Given an int n, return true if it is within 10 of 100 or 200.
+Note: Math.abs(num) computes the absolute value of a number.
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false
+ */
+public class Code7 {
+    public boolean nearHundred(int n) {
+        if (Math.abs(100-n)<=10 || Math.abs(200-n)<=10)
+            return true;
+        return false;
+    }
+}
