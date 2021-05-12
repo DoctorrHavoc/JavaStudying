bf9fca87225a531fbf03100741683cee1ecd0337
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2021-05-12 23:09:43 +0300

    Update sumDouble.java

--- CodingBatEx/Warmup-1/sumDouble.java
+++ CodingBatEx/Warmup-1/sumDouble.java
@@ -5,7 +5,7 @@ sumDouble(1, 2) → 3
 sumDouble(3, 2) → 5
 sumDouble(2, 2) → 8
  */
-public class Code1 {
+public class Code3 {
     public int sumDouble(int a,int b){
         if(a==b)
             return (a+b)*2;
