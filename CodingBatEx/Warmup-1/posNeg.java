1358bb077224c0912ff967374596121b6983747d
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2021-05-13 11:31:30 +0300

    Create posNeg.java

---
+++ CodingBatEx/Warmup-1/posNeg.java
@@ -0,0 +1,16 @@
+/*
+Given 2 int values, return true if one is negative and one is positive.
+Except if the parameter "negative" is true, then return true only if both are negative.
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+ */
+
+public class Code8{
+    public boolean posNeg(int a, int b, boolean negative){
+        if(negative)
+            return (a<0 && b<0);
+        else
+            return (a<0 && 0<b || 0<a && b<0);
+    }
