418354ba12204adf35b265600489d7e51ae40c88
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2021-06-30 22:47:45 +0300

    Create in3050.java

---
+++ CodingBatEx/Warmup-1/in3050.java
@@ -0,0 +1,16 @@
+/*
+Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+
+in3050(30, 31) → true
+in3050(30, 41) → false
+in3050(40, 50) → true
+ */
+
+public class Code26{
+    public boolean in3050(int a, int b){
+        if( (((30<=a)&&(a<=40)) && ((30<=b)&&(b<=40))) || (((40<=a)&&(a<=50)) && ((40<=b)&&(b<=50)))) return true;
+        return false;
+    }
+
+}
+
